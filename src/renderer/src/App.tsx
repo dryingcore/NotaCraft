7de67dd3201101bc90ecb3bb11bcca0b52b17aed
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 17:03:10 +0000

    Jul 14, 2024, 2:03 PM

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="flex h-full items-center justify-center">
+      <span className="text-4xl text-blue-900">Hello from electron</span>
     </div>
   )
 }
