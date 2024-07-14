ba094a319c3b219b306bb40bbbf4adf55617967e
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 17:22:10 +0000

    Jul 14, 2024, 2:22 PM

--- src/renderer/src/components/AppLayout.tsx
+++ src/renderer/src/components/AppLayout.tsx
@@ -5,6 +5,7 @@ export const SideBar = ({ className, children, ...props }: ComponentProps<'aside
   return (
     <aside
       className={(twMerge('w-[250px] mt-10 h-[100vh + 10px] overflow-auto'), className)}
+      {...props}
     ></aside>
   )
 }
