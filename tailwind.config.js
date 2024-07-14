2355e1eaae753bd6c515c5d48ce18d32c17cc14d
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 16:54:20 +0000

    Jul 14, 2024, 1:54 PM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/renderer/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
-
