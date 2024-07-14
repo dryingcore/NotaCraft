c6c32d68544c1c98e601cfb55359a94140c3789a
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 21:07:21 +0000

    Jul 14, 2024, 6:07 PM

---
+++ src/renderer/src/components/Button/ActionButtonsRow.tsx
@@ -0,0 +1,10 @@
+import { DeleteNoteButton, NewNoteButton } from '@/components'
+
+export const ActionsButtonsRow = () => {
+  return (
+    <div>
+      <NewNoteButton />
+      <DeleteNoteButton />
+    </div>
+  )
+}
