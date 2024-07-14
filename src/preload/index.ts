c83dd6cfb15e80ab8d7499979e297c616a8b713a
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 16:30:12 +0000

    Jul 14, 2024, 1:30 PM

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,2 +1,13 @@
-import { electronAPI } from '@electron-toolkit/preload'
 import { contextBridge } from 'electron'
+
+if (!process.contextIsolated) {
+  throw new Error('contextIsolation must be enabled in the BrowserWindow')
+}
+
+try {
+  contextBridge.exposeInMainWorld('context', {
+    // TODO
+  })
+} catch (error) {
+  console.error(error)
+}
