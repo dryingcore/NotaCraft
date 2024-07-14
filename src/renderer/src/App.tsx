1940e8852cf967318328d6521b80094e3af86215
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 21:11:30 +0000

    Jul 14, 2024, 6:11 PM

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,11 @@
-import { AppLayout, Content, SideBar } from '@/components'
+import { ActionsButtonsRow, AppLayout, Content, SideBar } from '@/components'
 
 const App = () => {
   return (
     <AppLayout>
-      <SideBar className="bg-slate-900">Sidebar Options</SideBar>
+      <SideBar className="bg-slate-900">
+        <ActionsButtonsRow className="flex justify-between mt-1" />
+      </SideBar>
       <Content className="border-l bg-gray-800 border-l-white">Content</Content>
     </AppLayout>
   )
