25b163dcd19e7054f0a31f94e3e52d0b4e7d689e
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-14 20:20:10 +0000

    Jul 14, 2024, 5:20 PM

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,10 +3,8 @@ import { AppLayout, Content, SideBar } from '@/components'
 function App() {
   return (
     <AppLayout>
-      <SideBar className="p-2" border-red-500>
-        SideBar
-      </SideBar>
-      <Content> Content </Content>
+      <SideBar className="p-2 border-4 border-red-800">SideBar</SideBar>
+      <Content className="border-4 border-blue-600">Content</Content>
     </AppLayout>
   )
 }
