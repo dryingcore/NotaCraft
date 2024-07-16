3ffbde12c3c0aeca77b7bc84be83f14319d35b67
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-16 17:50:55 +0000

    Jul 16, 2024, 2:50 PM

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -1,8 +1,17 @@
 import { notesMock } from '@/store/mocks'
 import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 import { NotePreview } from './NotePreview'
 
 export const NotePreviewList = ({ className, ...props }: ComponentProps<'ul'>) => {
+  if (!notesMock) {
+    return (
+      <ul className={twMerge('text-center pt-4', className)} {...props}>
+        <span>No notes yet!</span>
+      </ul>
+    )
+  }
+
   return (
     <ul {...props}>
       {notesMock.map((note) => (
--- src/renderer/src/store/mocks/index.ts
+++ src/renderer/src/store/mocks/index.ts
@@ -1,16 +1,3 @@
 import { NoteInfo } from '@shared/models'
 
-export const notesMock: NoteInfo[] = [
-  {
-    title: 'Note 1',
-    lastEditTime: new Date().getTime()
-  },
-  {
-    title: 'Note 2',
-    lastEditTime: new Date().getTime()
-  },
-  {
-    title: 'Note 3',
-    lastEditTime: new Date().getTime()
-  }
-]
+export const notesMock: NoteInfo[] = []
