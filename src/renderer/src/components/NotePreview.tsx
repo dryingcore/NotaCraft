34f27d4a4e1125dc67e09ead95388c0b45bc4d85
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-16 17:34:43 +0000

    Jul 16, 2024, 2:34 PM

--- src/renderer/src/components/NotePreview.tsx
+++ src/renderer/src/components/NotePreview.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@renderer/utils'
+import { cn, formateDateFromMS } from '@renderer/utils'
 import { NoteInfo } from '@shared/models'
 import { ComponentProps } from 'react'
 
@@ -14,6 +14,7 @@ export const NotePreview = ({
   className,
   ...props
 }: NotePreviewProps) => {
+  const date = formateDateFromMS(lastEditTime)
   return (
     <div
       className={cn(
@@ -24,7 +25,7 @@ export const NotePreview = ({
       {...props}
     >
       <h3 className="mb-1 font-bold truncate">{title}</h3>
-      <span className="inline-block w-full mb-2 text-xs font-light text-left">{lastEditTime}</span>
+      <span className="inline-block w-full mb-2 text-xs font-light text-left">{date}</span>
     </div>
   )
 }
