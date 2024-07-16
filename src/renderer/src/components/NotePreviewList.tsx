cf31a86eb98ed1e4d1060f974d59b53291835137
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-16 17:51:47 +0000

    Jul 16, 2024, 2:51 PM

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge'
 import { NotePreview } from './NotePreview'
 
 export const NotePreviewList = ({ className, ...props }: ComponentProps<'ul'>) => {
-  if (!notesMock) {
+  if (notesMock.length === 0) {
     return (
       <ul className={twMerge('text-center pt-4', className)} {...props}>
         <span>No notes yet!</span>
