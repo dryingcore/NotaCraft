663579d14882bca338d75e4d8fe0d992765a33bb
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-16 19:08:28 +0000

    Jul 16, 2024, 4:08 PM

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,11 +3,11 @@ import { ActionsButtonsRow, AppLayout, Content, NotePreviewList, SideBar } from 
 const App = () => {
   return (
     <AppLayout>
-      <SideBar className="bg-[#2B2A4C]">
+      <SideBar className="bg-[#001C30]">
         <ActionsButtonsRow className="flex justify-between mt-1" />
         <NotePreviewList className="mt-3, space-y-1" />
       </SideBar>
-      <Content className="border-l bg-[#232D3F] border-l-[#000000]">Content</Content>
+      <Content className="border-l bg-[#232D3F] border-l-[#001C30]">Content</Content>
     </AppLayout>
   )
 }
