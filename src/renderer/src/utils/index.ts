a610f9ef3a23227239cccc68552ed5b1c530c22f
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-16 17:47:45 +0000

    Jul 16, 2024, 2:47 PM

--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -1,7 +1,7 @@
 import { ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-const dateFormatter = new Intl.DateTimeFormat("pt-BR", {
+const dateFormatter = new Intl.DateTimeFormat(window.context.locale, {
   year: 'numeric',
   month: '2-digit',
   day: '2-digit',
