113ffeda5b1b5f5fe2c21cf31d547d44fc59c488
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-18 23:44:14 +0000

    Jul 18, 2024, 8:44 PM

--- src/renderer/src/components/index.ts
+++ src/renderer/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './AppLayout'
 export * from './Button'
+export * from './FloatingNoteTitle'
+export * from './MarkdownEditor'
 export * from './NotePreview'
 export * from './NotePreviewList'
-export * from './MarkdownEditor'
