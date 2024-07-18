4a9d97763b4b874e8e28e26b323d68a136a996e8
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-18 23:54:23 +0000

    Jul 18, 2024, 8:54 PM

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -3,3 +3,19 @@ import { NoteInfo } from '@shared/models'
 import { atom } from 'jotai'
 
 export const notesAtom = atom<NoteInfo[]>(notesMock)
+
+export const selectedNoteIndexAtom = atom<number | null>(null)
+
+export const selectedNoteAtom = atom((get) => {
+  const notes = get(notesAtom)
+  const selectedNoteIndex = get(selectedNoteIndexAtom)
+
+  if (!selectedNoteIndex) return null
+
+  const selectedNote = notes[selectedNoteIndex]
+
+  return {
+    ...selectedNote,
+    content: `# ${selectedNote.title}`
+  }
+})
