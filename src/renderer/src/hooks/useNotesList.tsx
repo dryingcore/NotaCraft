fe61352c6095dd0f51b4de8664cac918a2aa4224
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-19 00:00:57 +0000

    Jul 18, 2024, 9:00 PM

---
+++ src/renderer/src/hooks/useNotesList.tsx
@@ -0,0 +1,7 @@
+import { notesAtom, selectedNoteIndexAtom } from '@/store'
+import { useAtom, useAtomValue } from 'jotai'
+
+export const useNotesList = () => {
+  const notes = [useAtomValue(notesAtom)]
+  const [selectedNoteIndex, setSelectedNoteIndex] = useAtom(selectedNoteIndexAtom)
+}
