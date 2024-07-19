17d3979368ef4ca1c77d86b1b3f715542dde2b87
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-19 12:49:58 +0000

    Jul 19, 2024, 9:49 AM

--- src/renderer/src/store/mocks/index.ts
+++ src/renderer/src/store/mocks/index.ts
@@ -4,17 +4,5 @@ export const notesMock: NoteInfo[] = [
   {
     title: 'Note 1',
     lastEditTime: new Date().getTime()
-  },
-  {
-    title: 'Note 2',
-    lastEditTime: new Date().getTime()
-  },
-  {
-    title: 'Note 3',
-    lastEditTime: new Date().getTime()
-  },
-  {
-    title: 'Note 4',
-    lastEditTime: new Date().getTime()
   }
 ]
