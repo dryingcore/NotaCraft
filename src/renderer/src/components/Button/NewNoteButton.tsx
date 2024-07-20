a11a0f36a4e00c74a0ff93b98236c0fdff567fe4
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-20 21:37:40 +0000

    Jul 20, 2024, 6:37 PM

--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -2,6 +2,9 @@ import { ActionButton, ActionButtonProps } from '@/components'
 import { LuFileSignature } from 'react-icons/lu'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
+  const createEmptyNote = () => {
+  }
+
   return (
     <ActionButton className="hover:bg-green-600" {...props}>
       <LuFileSignature className="w-4 h-4 text-white" />
