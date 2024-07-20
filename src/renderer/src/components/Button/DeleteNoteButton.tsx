36fb4ffd62e82d934ec1fe754beda395a6d85032
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-20 21:41:19 +0000

    Jul 20, 2024, 6:41 PM

--- src/renderer/src/components/Button/DeleteNoteButton.tsx
+++ src/renderer/src/components/Button/DeleteNoteButton.tsx
@@ -1,9 +1,16 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { deleteNoteAtom } from '@renderer/store'
+import { useSetAtom } from 'jotai'
 import { LuTrash2 } from 'react-icons/lu'
 
 export const DeleteNoteButton = ({ ...props }: ActionButtonProps) => {
+  const deleteNote = useSetAtom(deleteNoteAtom)
+  const handleDeletion = () => {
+    deleteNote()
+  }
+
   return (
-    <ActionButton className="hover:bg-red-600" {...props}>
+    <ActionButton onClick={handleDeletion} className="hover:bg-red-600" {...props}>
       <LuTrash2 className="w-4 h-4 text-white" />
     </ActionButton>
   )
--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,12 +1,16 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { createEmptyNoteAtom } from '@renderer/store'
+import { useSetAtom } from 'jotai'
 import { LuFileSignature } from 'react-icons/lu'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
-  const createEmptyNote = () => {
+  const createEmptyNote = useSetAtom(createEmptyNoteAtom)
+  const handleCreation = () => {
+    createEmptyNote()
   }
 
   return (
-    <ActionButton className="hover:bg-green-600" {...props}>
+    <ActionButton onClick={handleCreation} className="hover:bg-green-600" {...props}>
       <LuFileSignature className="w-4 h-4 text-white" />
     </ActionButton>
   )
