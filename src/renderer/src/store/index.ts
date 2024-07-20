9e05496cece53a45ceb57a102273c287240836b7
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-20 21:35:52 +0000

    Jul 20, 2024, 6:35 PM

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -6,7 +6,7 @@ export const notesAtom = atom<NoteInfo[]>(notesMock)
 
 export const selectedNoteIndexAtom = atom<number | null>(null)
 
-export const selectedNoteAtom = atom(async (get) => {
+export const selectedNoteAtom = atom((get) => {
   const notes = get(notesAtom)
   const selectedNoteIndex = get(selectedNoteIndexAtom)
 
@@ -28,4 +28,19 @@ export const createEmptyNoteAtom = atom(null, (get, set) => {
   }
 
   set(notesAtom, [...notes.filter((note) => note.title !== newNote.title), newNote])
+  set(selectedNoteIndexAtom, 0)
+})
+
+export const deleteNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+  const selectedNote = get(selectedNoteAtom)
+
+  if (!selectedNote) return
+
+  set(
+    notesAtom,
+    notes.filter((note) => note.title !== selectedNote.title)
+  )
+
+  set(selectedNoteIndexAtom, null)
 })
